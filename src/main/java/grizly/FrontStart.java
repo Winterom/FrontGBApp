ced85e998f2ae0a9cd51c7449af031dc06670064
Winterom <winterom@gmail.com>
2022-06-27 14:02:46 +0300

    Initial commit

---
+++ src/main/java/grizly/FrontStart.java
@@ -0,0 +1,11 @@
+package grizly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontStart {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontStart.class,args);
+    }
+}
